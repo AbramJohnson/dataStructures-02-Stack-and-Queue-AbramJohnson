c4e8c23d816c048417e6b695e1a4fa3942400723
Abram Johnson <abram.johnson@g.austincc.edu>
2023-10-25 12:49:52 -0500

    created class SNode

--- data.h
+++ data.h
@@ -1,10 +1,19 @@
 /*************************
  * Name: Abram Johnson
  * dataStructure 02
- * Purpose: Create new data type and define data class
+ * Purpose: Create new data type and define SNode class
 **************************/
 
 #ifndef Data_H
 #define DATA_H
+
+template <typename T>
+class SNode {
+    private:
+    T data;
+    SNode<T> *next;
+
+    friend class linkedlist<T>;
+};
 
 #endif
